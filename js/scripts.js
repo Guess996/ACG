d778ff4cde47adca7a77b488b0c073c4d607c6cc
Guess996 <61116196+Guess996@users.noreply.github.com>
2021-11-11 17:25:13 +0800

    ACG

--- js/scripts.js
+++ js/scripts.js
@@ -55,15 +55,22 @@
     SearchButton.onclick=()=>{
         if(textSearch.value==''){
             alert('请输入内容');
+            console.log(document.body.scrollWidth);
         }else{
-            location.replace('https://www.baidu.com/s?word='+textSearch.value+'&sa=tb&ts=7206220&t_kt=0&ie=utf-8&rsv_t=9588S0aIZpfQAnhoSE7MgRU5t4Zz0VU2LDEFf50%252Fvl5v71yW2eVHwFqiCg&ms=1&rsv_pq=10428263317001595691&ss=110&sugid=14743310232227904645&rqlang=zh&rsv_sug4=8077&inputT=6832&oq=a')
+            if(document.body.scrollWidth<=800){
+                location.replace('https://www.baidu.com/s?word='+textSearch.value+'&sa=tb&ts=7206220&t_kt=0&ie=utf-8&rsv_t=9588S0aIZpfQAnhoSE7MgRU5t4Zz0VU2LDEFf50%252Fvl5v71yW2eVHwFqiCg&ms=1&rsv_pq=10428263317001595691&ss=110&sugid=14743310232227904645&rqlang=zh&rsv_sug4=8077&inputT=6832&oq=a')
+            }else{
+                location.replace('https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&ch=&tn=baidu&bar=&wd='+textSearch.value+'&rn=&fenlei=256&oq=&rsv_pq=aeedc18f0001200d&rsv_t=4c9ajItCpBIRzc%2FYcTjncIRpgEoyk0pNhN%2Fd6amXepNU2tAXWzLUhdkTRZQ&rqlang=cn&rsv_enter=0&rsv_btype=i&rsv_dl=ib&inputT=1622')
+            }
+            
         }
     }
     const social=document.querySelectorAll('#social>a')
     for(let i=0;i<3;i++ ){
         social[i].onclick=()=>{
             switch(i){
-                case 2:location.replace('https://%72%65%73%2e%61%62%65%69%6d%2e%63%6e/%61%70%69/%71%71/?%71%71=10001')
+                //https://%72%65%73%2e%61%62%65%69%6d%2e%63%6e/%61%70%69/%71%71/?%71%71=10001
+                case 2:location.replace('https://qm.qq.com/cgi-bin/qm/qr?k=DheR_SS89gXmeZizNGWcxpwfVMmLA1aY&jump_from=webapi')
                 break;
                 default:alert('没有')
             }
