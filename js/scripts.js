3b13c4ed9b28f59140fe42fe6fe57b56f4ff389e
Guess996 <61116196+Guess996@users.noreply.github.com>
2022-04-16 19:29:53 +0800

    ACG

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,40 @@
 // 
     window.addEventListener('DOMContentLoaded', () => {
 
+    var t = document.title ;
+    var t01 = t + "♪♫¸（ >‿◠）✌";
+    var t02 = t + "♪♫¸（⊙‿⊙）✌";
+    var t03 = t + "♪♫¸（◠▽◠）✌";
+    var t04 = t + "♪♫¸（ ô‿ô ） ✌";
+    var t05 = t + "♪♫¸（͡° ʖ ͡°）✌";
+    var t06 = t + "♪♫¸（͡ Ö‿Ö ） ✌";
+    var t07 = t + "♪♫¸（＾▽＾）✌";
+    var t08 = t + "♪♫¸（ •◡• ）✌";
+    var t09 = t + "♪♫¸（ ¬‿¬ ）✌";
+    var t10 = t + "♪♫¸（◉◡◉）✌";
+    var t11 = t + "♪♫¸（◔◡◔）✌";
+    var t12 = t + "♪♫¸（ˇωˇ）✌";
+    var t13 = t + "♪♫¸（ô ◡ ô）✌";
+    var t14 = t + "♪♫¸（∩▽∩）✌";
+    var t15 = t + "♪♫¸（⊙△⊙）✌";
+    var t16 = t + "♪♫¸（≧▽≦）✌";
+    var t17 = t + "♪♫¸（ ^人^ ）✌";
+    var t18 = t + "♪♫¸（°ω°）✌";
+    var t19 = t + "♪♫¸（ˋωˊ）✌";
+    var t20 = t + "♪♫¸（ˋ△ˊ）✌";
+    var t21 = t + "♪♫¸（ˇ▽ˇ）✌";
+    var t22 = t + "♪♫¸（°ο°）✌";
+    var t23 = t + "♪♫¸（ˇ◡ˇ）✌";
+    var t24 = t + "♪♫¸（ ⊙ʖ⊙）✌";
+    var t25 = t + "♪♫¸（ˉ▽ˉ）✌";
+    var t26 = t + "♪♫¸（￣□￣）✌";
+    var myObj = {"title":[ t01,t02,t03,t04,t05,t06,t07,t08,t09,t10,t11,t12,t13,t14,t15,t16,t17,t18,t19,t20,t21,t22,t23,t24,t25,t26 ]}
+    var x = myObj.title.length;
+    var i = 0 ;
+    function tit(){document.getElementsByTagName("title")[0].innerText = myObj.title[i];i++;if( i == x ){ i = 0 ;}setTimeout(function (){tit()}, 1000);}
+    tit()
+
+
     // Navbar shrink function
     var navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
